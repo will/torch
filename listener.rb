b83644cc72a22194b96e675e7681f18bffb0ce24
Will Leinweber <will@bitfission.com>
2011-11-26 17:17:35 -0800

    parse

--- listener.rb
+++ listener.rb
@@ -1,9 +1,46 @@
 require 'eventmachine'
+require 'strscan'
 
 module Printer
-  def receive_data(data)
-    p data
+  class Log
+    def initialize(data)
+      scan(data)
+      scan_router(@log) if @ps == 'router'
+    end
+
+    private
+    def scan(str)
+      ss = StringScanner.new(str)
+      ss.skip_until(/\s/)
+      ss.skip_until(/\s/)
+      @date = ss.scan_until(/\s/).strip
+      @token = ss.scan_until(/.\s/).strip
+      @service = ss.scan_until(/.\s/).strip
+      @ps = ss.scan_until(/.\s/).strip
+      ss.skip_until(/- - /)
+      @log = ss.rest.strip
+    end
+
+    def scan_router(str)
+      result = Hash[ str.split(/ /).map{|j| j.split(/=/)} ]
+      @queue = result['queue']
+      @wait = result['wait']
+      @service = result['service']
+    end
+
+    def print
+      if @ps == 'router'
+        puts "\n#{@token} queue: #{@queue} wait: #{@wait} service: #{@service}"
+      else
+        print '.'
+      end
+    end
   end
+
+  def receive_data(data)
+    Log.new(data).print
+  end
+
 end
 
 EventMachine::run do
